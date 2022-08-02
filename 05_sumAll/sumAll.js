d383cb97c62be1d64f41b1c802260c595e1e25b2
imalreadyafiddle <christopher.m.davis90@gmail.com>
2022-08-01 20:13:41 -0400

    Completed JS Exercises 6 & 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,17 @@
+// take two numbers (e.g., 3, 6), add them and all the numbers in between (3 + 4 + 5 + 6), return sum
+// script returns ERROR if a number is negative or is not a number
+// non-integers will give wonky results
 const sumAll = function(numOne, numTwo) {
     let sum = 0;
     let twoNumbers = [numOne, numTwo];
     if (numOne > 0 && numTwo > 0 && typeof(numOne) === 'number' && typeof(numTwo) === 'number') {
-        // sort but off of numerical value and not the stupid default unicode value, don't ask me why this works, it just does
+        // sort, but off of numerical value and not the stupid default unicode value
+        // don't ask me why this works, it just does
         twoNumbers.sort(function(a,b) {
             return a - b;
         });
-       // assign value of lower number to i, add i to sum and increment i by one until higher number is reached
+       // assign value of lower number to i, add i to sum and increment i by one 
+       // until higher number is reached
         for (i = twoNumbers[0]; i <= twoNumbers[1]; i++) {
             sum += i;
             // almost forgot to actually return something
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+// take a farenheit temp, return it as celsius, rounding to one decimal point if needed
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5/9);
+  return Math.round(cTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+// take a celsius temp, return it as farenheit, rounding to one decimal point if needed
+const ctof = function(cTemp) {
+  let fTemp = (cTemp * 9/5 + 32);
+  return Math.round(fTemp * 10) / 10;
 };
 
 // Do not edit below this line
