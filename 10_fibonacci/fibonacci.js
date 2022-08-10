d9251731d5795e748afc6c38e0cbb384fe39ad9b
imalreadyafiddle <christopher.m.davis90@gmail.com>
2022-08-10 10:08:32 -0400

    Completed JS exercises 10-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function(fibTo) {
-    
+const fibonacci = function(num) {
+    num = parseInt(num)
+    if (typeof(num) !== "number" || num <= 0) {return "OOPS"}
+    var n1 = 1, n2 = 0, temp;
+    while (num > 0) {
+        temp = n1;
+        n1 = n1 + n2;
+        n2 = temp;
+        num--;
+    }; return n2;
 };
 
 // Do not edit below this line
